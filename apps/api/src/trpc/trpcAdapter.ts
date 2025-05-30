72b326083b947667ab067dd7e26791911bba87d0
endabelyu <endabelyuproject@gmail.com>
2025-05-30 21:30:32 +0700

    fix:: - try too fix unavailabel module

--- apps/api/src/trpc/trpcAdapter.ts
+++ apps/api/src/trpc/trpcAdapter.ts
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { Hono } from "hono";
-import { appRouter } from "@repo/trpc";
+import { appRouter } from "../../../../packages/trpc";
 
 const trpcRoute = new Hono();
 
